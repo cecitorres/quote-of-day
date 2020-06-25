90b866fab1b6938b9d049b86f0cbbb82558bcdf5
Ceci Torres <cecitorres7@gmail.com>
2020-06-24 20:57:34 -0500

    delete fav quote

--- src/views/FavoriteQuotesPage.js
+++ src/views/FavoriteQuotesPage.js
@@ -9,25 +9,23 @@ const FavoriteQuotesPage = () => {
 
   useFirestoreConnect("quotes");
   const quotes = useSelector((state) => state.firestore.data.quotes);
-  // console.log(Object.entries(quotes));
 
   const deleteFavQuote = useCallback(
-		idQuote => {
-			return dispatch(deleteQuote({ firestore }, idQuote))
-		},
+  	idQuote => {
+  		return dispatch(deleteQuote({ firestore }, idQuote))
+  	},
     [firestore]
-	);
+  );
   return (
     <>
-      {Object.entries(quotes)} 
       {quotes &&
-        Object.values(quotes).map((quote) => (
-          <div key={quote.id}>
-            <p className="mb-0">{quote.body}</p>
+        Object.entries(quotes).map((quote) => (
+          <div key={quote[0]}>
+            <p className="mb-0">{quote[1].body}</p>
             <div className="align-items-end text-center text-wrap">
-              <span>-- {quote.author}</span>
+              <span>-- {quote[1].author}</span>
               <button
-                onClick={() => deleteFavQuote(quote.id)}
+                onClick={() => deleteFavQuote(quote[0])}
                 type="button"
                 className="btn btn-warning btn-sm ml-3 rounded-circle float-right"
               >
